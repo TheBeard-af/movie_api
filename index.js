97aeaeae7cc18c1f323f7f793e1dc4a422be656a
TheBeard-af <afaqnadeem1993@gmail.com>
2025-01-22 10:59:58 +1100

    Added movies and port listening

--- index.js
+++ index.js
@@ -11,7 +11,28 @@ let movies = [
       title: 'Lord of the Rings'
     },
     {
+      title: 'Rush'
+    },
+    {
+      title: 'Click'
+    },
+    {
+      title: 'Speed'
+    },
+    {
+      title: 'The Matrix'
+    },
+    {
       title: 'Twilight'
+    },
+    {
+      title: 'Terminator 2: Judgment Day'
+    },
+    {
+      title: 'If'
+    },
+    {
+      title: 'Oblivion'
     }
   ];
 
@@ -32,3 +53,9 @@ let movies = [
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
+
+  const port = process.env.PORT || 8080;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port' + port);
+});
