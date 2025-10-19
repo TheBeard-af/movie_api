1ef049ca587b038115f26a7318259c4a99709c2f
TheBeard-af <afaqnadeem1993@gmail.com>
2025-10-19 17:22:12 +1100

    Remove JWT authentication from /movies endpoint for React connection

--- index.js
+++ index.js
@@ -59,20 +59,16 @@ const passport = require("passport");
 require("./passport");
 
 // Get all Movies
-app.get(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    await Movies.find()
-      .then((movies) => {
-        res.status(200).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error:" + err);
-      });
-  }
-);
+app.get("/movies", async (req, res) => {
+  await Movies.find()
+    .then((movies) => {
+      res.status(200).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error:" + err);
+    });
+});
 
 // Return movie data for one movie by title
 app.get(
